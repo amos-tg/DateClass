57a9a38114ccb1f54f196c30eac7b1b61c165a9e
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2026-03-09 16:04:41 -0400

    Leap year setDate test, added test msg for all fns
    some of the functions have overloads which are identical to each other
    in all but argument passing vs member usage; I am going to use only one
    test for those functions.

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,16 @@ using namespace std;
 
 const char *TEST_PASS = "Passed";
 
+const char *YMD_VALIDATE_TEST = "Test (ymdValidate method): ";
 const char *DEFAULT_TEST = "Test (Default Constructor): ";
 const char *SET_DATE_TEST = "Test (setDate method): ";
 const char *GET_DATE_MDY_NUM_TEST = "Test (getDateMDYNum method): ";
 const char *GET_DATE_MDY_ALPH_NUM_TEST = "Test (getDateMDYAlphNum): ";
 const char *GET_DATE_DMY_ALPH_NUM_TEST = "Test (getDateDMYAlphNum): ";
+const char *SET_DATE_LEAP_TEST = "Test (setDate method on leap year): ";
+const char *LAST_DAY_TEST = "Test (lastDay method): ";
+const char *LAST_DAY_LEAP_TEST = "Test (lastDay method on leap year): ";
+const char *TO_STR_MONTH_TEST = "Test (toStrMonth method): ";
 
 int main(void) {
   cout << DEFAULT_TEST;
@@ -39,6 +44,15 @@ int main(void) {
   cout << GET_DATE_DMY_ALPH_NUM_TEST;
   assert(tested.getDateDMYAlphNum() == string("5 August, 2026"));
   cout << TEST_PASS << endl;
+
+  // this internally tests the isLeapYear function which has the same logic in
+  // it's overload, so I will omit the tests for those.
+  cout << SET_DATE_LEAP_TEST;
+  tested.setDate(2004, FEBRUARY, 29);
+  assert(tested.getDateMDYNum() == string("2/29/2004"));
+  cout << TEST_PASS << endl;
+
+  int lastDay
 
   return 0;
 }
